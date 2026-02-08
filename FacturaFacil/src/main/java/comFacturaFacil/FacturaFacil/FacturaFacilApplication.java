3451e2bbc4b6938af902d12c41e0423cf4c2690e
CesArB239 <csanz4926@gmail.com>
2026-02-08 22:11:54 +0100

    Archivo Basico Facturacion

---
+++ FacturaFacil/src/main/java/comFacturaFacil/FacturaFacil/FacturaFacilApplication.java
@@ -0,0 +1,13 @@
+package comFacturaFacil.FacturaFacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturaFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturaFacilApplication.class, args);
+	}
+
+}
---
+++ FacturaFacil/src/test/java/comFacturaFacil/FacturaFacil/FacturaFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package comFacturaFacil.FacturaFacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturaFacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
